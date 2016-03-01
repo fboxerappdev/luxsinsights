9b97aeef480adfbfdc82645f441ac83fd1b3059a
fboxer <freezingboxer1@gmail.com>
2016-03-01 18:50:38 +0530

    left bar done

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -1,3 +1,11 @@
 /*
- grunt-getting-started 2016-02-25 
-*/
+ grunt-getting-started 2016-02-25
+ */
+
+
+
+$(document).ready(function(){
+  $(".toggle").click(function(){
+   $(".left-bar,.display-bar").toggleClass("open");
+  });
+});
