d19c83c068192fbc3117ee3095816e30e77b1fd4
fboxer <freezingboxer1@gmail.com>
2016-03-02 18:51:53 +0530

    page done

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -5,7 +5,52 @@
 
 
 $(document).ready(function(){
-  $(".toggle").click(function(){
-   $(".left-bar,.display-bar").toggleClass("open");
-  });
+
+
+         var winwid= $(document).width();
+
+        if(winwid>768){
+             $(".toggle").click(function(){
+              $(".left-bar,.display-bar").toggleClass("open");
+             });
+        }
+
+        if(winwid<768){
+            $(".left-bar,.display-bar").toggleClass("open");
+
+            $(".toggle").click(function(){
+                $(".left-bar,.display-bar").toggleClass("open");
+            });
+        }
+
+
+
+    var data = {
+        labels: ["Financial Performance", "Health", "Environmental Sustainability", "Safety", "Usability"],
+        datasets: [
+            {
+                label: "My Second dataset",
+                fillColor: "rgba(34,39,82,0.9)",
+                strokeColor: "rgba(151,187,205,1)",
+                pointColor: "rgba(151,187,205,1)",
+                pointStrokeColor: "#fff",
+                pointHighlightFill: "#fff",
+                pointHighlightStroke: "rgba(151,187,205,1)",
+                data: [5, 50, 5, 25, 30]
+            }
+        ]
+    };
+
+    var ctx = $("#myChart").get(0).getContext("2d");
+    // This will get the first returned node in the jQuery collection.
+
+
+
+    var myRadarChart = new Chart(ctx).Radar(data, {
+        pointDot : false
+    });
+
+
+
+
 });
