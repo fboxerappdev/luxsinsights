8d27aeeaae483d17a625cc38590431cf14788a3c
fboxer <freezingboxer1@gmail.com>
2016-04-28 16:38:21 +0530

    new page done

--- dist/js/custom.js
+++ dist/js/custom.js
@@ -6,28 +6,11 @@
 
 $(document).ready(function() {
 
+    $('[data-toggle="popover"]').popover();
 
     var winwid = $(document).width();
 
 
-  //  var headerHi = $('header').outerHeight();
-
-    //$("<div></div>").css({
-    //    width:"100%",
-    //    height:headerHi+"px"
-    //}).insertAfter("header");
-
-
-   // var footerHi = $('footer').outerHeight();
-
-
-
-   // var displayHi = $('.display-bar').outerHeight();
-
-   // var contentHi = displayHi-headerHi-footerHi-10;
-
-
-    //$(".content-box").css("height",contentHi);
 
     $(".toggle").click(function(){
         $(".left-bar,.display-bar").toggleClass("open");
@@ -61,16 +44,46 @@ $(document).ready(function() {
 
 
 
+    $(".chart-list-item").on("click",function(){
+        $(".chart-list-item").removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+
+        $(".main-detail-item").hide();
+        $(".main-detail-item:eq('"+index+"')").fadeIn();
+    });
+
+
+
     $('.chart').easyPieChart({
         lineWidth:40,
         barColor: '#4D5360',
         trackColor:"#EEEEEE",
         lineCap:"square",
-        scaleColor:"#fff",
+        scaleColor:"transparent",
         size:"250",
         onStep: function(from, to, percent) {
             $(this.el).find('.percent').text(Math.round(percent));
         }
     });
+
+    function piechart(selecter,color){
+        $(selecter).easyPieChart({
+            lineWidth:50,
+            barColor: color,
+            trackColor:"#EEEEEE",
+            lineCap:"square",
+            scaleColor:"transparent",
+            size:"300",
+            onStep: function(from, to, percent) {
+                $(this.el).find('.percent').text(Math.round(percent));
+            }
+        });
+    }
+    piechart("#financial","#FDB45C");
+    piechart("#health","#46BFBD");
+    piechart("#sustainability","#ACD651");
+    piechart("#safety","#949FB1");
+    piechart("#usability","#AE80BA");
 
 });
